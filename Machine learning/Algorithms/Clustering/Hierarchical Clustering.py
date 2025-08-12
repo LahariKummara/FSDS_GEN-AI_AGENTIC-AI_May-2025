d89995f4a1437608e0494ac3ed7b2366c2e93d26
Kummara Lahari <lahari11kummara@gmail.com>
2025-08-12 20:05:46 +0530

    Add files via upload

---
+++ Machine learning/Algorithms/Clustering/Hierarchical Clustering.py
@@ -0,0 +1,42 @@
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+dataset = pd.read_csv(r'C:\Users\LAHARI\OneDrive\Documents\Mall_Customers.csv')
+x = dataset.iloc[:,[3,4]].values
+
+
+import scipy.cluster.hierarchy as sch
+
+dendrogram = sch.dendrogram(sch.linkage(x, method = 'ward'))
+
+
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean distances')
+plt.show()
+
+
+
+# Training the Hierarchical Clustering model on the dataset
+
+from sklearn.cluster import AgglomerativeClustering
+hc = AgglomerativeClustering(n_clusters = 5, linkage = 'ward')
+y_hc = hc.fit_predict(x)
+
+
+# Visualizing the Clusters
+plt.scatter(x[y_hc == 0,0],  x[y_hc == 0,1],s = 100, c = 'red', label = 'Cluster 1')
+plt.scatter(x[y_hc == 1, 0], x[y_hc == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
+plt.scatter(x[y_hc == 2, 0], x[y_hc == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
+plt.scatter(x[y_hc == 3, 0], x[y_hc == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
+plt.scatter(x[y_hc == 4, 0], x[y_hc == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
+plt.title('Clusters of Customers')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.show()
+
+dataset['cluster'] = y_hc
